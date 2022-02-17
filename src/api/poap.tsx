55e1087457a2c15b4e1f5679b354784fd89d11c5
JSantos92 <skyjgod@gmail.com>
2022-02-17 04:34:36 +0000

    Changed api call to https

--- src/api/poap.tsx
+++ src/api/poap.tsx
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const getEvents = async (query: string) => {
-  const path = `http://api.poap.xyz/events?name=${query}`;
+  const path = `https://api.poap.xyz/events?name=${query}`;
   const res = await axios.get(path);
   if (res) {
     console.log('Get events service:', res);
