387d3cbb3f4472748a66e48fa2a9de137e8162fd
JSantos92 <skyjgod@gmail.com>
2022-02-18 08:38:12 +0000

    landing image

--- src/views/Landing.tsx
+++ src/views/Landing.tsx
@@ -1,17 +1,24 @@
 import { Box, Typography } from '@mui/material';
 
 import styled from 'styled-components';
+import Portudao from 'icons/portudao.jpg';
+import spacing from 'theme/spacing';
 
 const Container = styled(Box)`
   display: flex;
+  flex-direction: column;
   justify-content: center;
   align-items: center;
+  h3 {
+    margin-bottom: ${spacing.xxl}px;
+  }
 `;
 
 export default function Landing(): JSX.Element {
   return (
     <Container>
       <Typography variant="h3"> Landing </Typography>
+      <img src={Portudao} alt="" />
     </Container>
   );
 }
